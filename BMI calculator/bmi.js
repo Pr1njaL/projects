3664e334c2dd21dbf92277892cb7f4513b78b492
Pr1njaL <2025ugee108@nitjsr.ac.in>
2026-01-30 23:11:13 +0530

    Update bmi.js

--- BMI calculator/bmi.js
+++ BMI calculator/bmi.js
@@ -5,32 +5,33 @@ function calculateBMI() {
     const bmiValue = document.getElementById('bmi-value');
     const bmiStatus = document.getElementById('bmi-status');
 
-    // Validation
+    
     if (weight === "" || height === "") {
         alert("Please enter both weight and height!");
         return;
     }
 
-    // BMI Formula: weight (kg) / (height (m) * height (m))
+    
     const heightInMeters = height / 100;
     const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(1);
 
-    // Update UI
+    
     resultBox.style.display = "block";
     bmiValue.innerText = bmi;
 
-    // Determine Status
+    
     if (bmi < 18.5) {
         bmiStatus.innerText = "Underweight";
-        bmiStatus.style.color = "#e67e22"; // Orange
+        bmiStatus.style.color = "#e67e22"; 
     } else if (bmi >= 18.5 && bmi < 24.9) {
         bmiStatus.innerText = "Normal Weight";
-        bmiStatus.style.color = "#2ecc71"; // Green
+        bmiStatus.style.color = "#2ecc71"; 
     } else if (bmi >= 25 && bmi < 29.9) {
         bmiStatus.innerText = "Overweight";
-        bmiStatus.style.color = "#e67e22"; // Orange
+        bmiStatus.style.color = "#e67e22"; 
     } else {
         bmiStatus.innerText = "Obese";
         bmiStatus.style.color = "#e74c3c"; // Red
     }
+
 }
